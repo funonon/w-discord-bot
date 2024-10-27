3cd384e2b2948bfcfe4cd42e125e5b3aabd0385b
funonon <funo222@outlook.jp>
2024-10-27 22:31:45 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,23 @@
 import discord
 from discord.ext import commands
 import os
+from keep_alive import keep_alive  # 追加
 
-# Intents設定（メンバー関連の情報にアクセスする場合は適切なIntentsを有効にする必要があります）
+# Botの設定
 intents = discord.Intents.default()
 intents.message_content = True
+bot = commands.Bot(command_prefix="!", intents=intents)
 
-# Botの接続設定
-bot = commands.Bot(command_prefix="!", intents=intents)  # プレフィックスを任意で設定
-
-# Botの起動時メッセージ
 @bot.event
 async def on_ready():
     print(f"Bot is online as {bot.user}")
 
-# シンプルなpingコマンド
 @bot.command()
 async def ping(ctx):
     await ctx.send("Pong!")
 
-# Discordのボットトークンを環境変数から取得して起動
+# サーバーの起動
+keep_alive()  # 追加
+
+# Discordのボットトークンを使って起動
 bot.run(os.getenv("DISCORD_TOKEN"))
